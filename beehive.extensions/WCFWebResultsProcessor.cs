d0be9fe5f4d6d0827786c3b50e339dbaf68b2d54
Jason Brown <jasonbrown2k3@yahoo.com>
2015-01-24 08:55:28 -0600

    added fleck websocket for notifications

--- beehive.extensions/WCFWebResultsProcessor.cs
+++ beehive.extensions/WCFWebResultsProcessor.cs
@@ -1,6 +1,8 @@
 ﻿using beehive.common.Contracts;
 using beehive.common.Objects;
 using beehive.extensions.Notifications;
+using Fleck;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,27 +16,58 @@ namespace beehive.extensions
 {
     public class WCFWebResultsProcessor : IResultProcessor
     {
-        private const string SERVICE_URI = @"http://localhost:8335/";
-        private readonly ServiceEndpoint svcEndpoint;
-        private readonly WebServiceHost svcHost;
+        private ILog log = LogManager.GetLogger(typeof(WCFWebResultsProcessor));
+
+        private const string NOTIFICATION_SERVICE_URI = @"http://localhost:8335/";
+        private const string SOCKET_SERVICE_URI = @"ws://0.0.0.0:8336/";
+
+        private readonly ServiceEndpoint notificationEndpoint;
+        private readonly WebServiceHost notificationHost;
+        private readonly WebSocketServer server;
+        private readonly List<IWebSocketConnection> allSockets;
+
         public WCFWebResultsProcessor(IDisk disk)
         {
-            Uri baseAddress = new Uri(SERVICE_URI);
-            svcHost = new WebServiceHost(typeof(NotificationHost));
-            svcHost.Description.Behaviors.Add(new MyServiceBehavior(() => new NotificationHost(disk)));
-            svcEndpoint = svcHost.AddServiceEndpoint(typeof(INotificationHost),new WebHttpBinding(), baseAddress);
-            svcEndpoint.Behaviors.Add(new WebHttpBehavior());
+            Uri notificationAddress = new Uri(NOTIFICATION_SERVICE_URI);
+            notificationHost = new WebServiceHost(typeof(NotificationHost));
+            notificationHost.Description.Behaviors.Add(new MyServiceBehavior(() => new NotificationHost(disk)));
+            notificationEndpoint = notificationHost.AddServiceEndpoint(typeof(INotificationHost), new WebHttpBinding(), notificationAddress);
+            notificationEndpoint.Behaviors.Add(new WebHttpBehavior());
 
-            svcHost.Open();
+            notificationHost.Open();
+
+            server = new WebSocketServer(SOCKET_SERVICE_URI);
+            allSockets = new List<IWebSocketConnection>();
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    log.Debug("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    log.Debug("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    log.Debug(message);
+                    allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+                };
+            });
+
         }
         public void Process(CommandResult result)
         {
-            throw new NotImplementedException();
+            allSockets.ForEach(s => s.Send(result.Message));
         }
 
         public void Dispose()
         {
-            if (svcHost != null) svcHost.Close();
+            if (notificationHost != null) notificationHost.Close();
+            if (allSockets.Any()) allSockets.ForEach(s => s.Close());
+            if (server != null) server.Dispose(); 
         }
     }
 }
